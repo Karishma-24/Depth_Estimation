6dc98a5164cea148ba759c794bdd3eb752b91e25
Karishma-24 <karishmakrish24@gmail.com>
2026-05-16 15:42:11 +0400

    updates final - 1

--- readingpkl.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open(r"C:\Users\KARISHMA\Desktop\deep learning\Depth_Estimation\training_outputs\training_11-24april resnet+unet 50k dataset 2.0\all_results_v8.pkl", "rb") as f:
-    data = pickle.load(f)
-
-print(data)
